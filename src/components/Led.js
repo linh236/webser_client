1c204106e7384d94f0306ee1fc92758ea9f5c083
LNQ <linhnq36@gmail.com>
2020-11-06 03:06:15 +0700

    auto run change background button status

--- src/components/Led.js
+++ src/components/Led.js
@@ -8,6 +8,7 @@ import DateTimePicker from 'react-datetime-picker';
 import Modal from 'react-bootstrap/Modal'
 import Container from '@material-ui/core/Container';
 import $ from 'jquery';
+import Clock from 'react-clock';
 function Led (){
   const [error, setError] = useState(null);
   const [isLoading, setLoading] = useState(true);
@@ -25,6 +26,7 @@ function Led (){
   const [name, setName] = useState('');
   const [column, setColumn] = useState('');
   const [date, setDate] = useState(new Date());
+   const [clock, setClock] = useState(new Date());
 
   const [show, setShow] = useState(false);
 
@@ -33,7 +35,15 @@ function Led (){
   let id = localStorage.getItem('id');
 
    useEffect(() => {
-     getLed(id)
+     getLed(id);
+     const interval = setInterval(
+      () => setClock(new Date()),
+      1000
+    );
+
+    return () => {
+      clearInterval(interval);
+    }
    },[])
 
 
@@ -117,6 +127,88 @@ function Led (){
 
   const autorun = () => {
     // $(".led_status0").addClass('bg-info');
+    let format_clock = moment(clock).format('Y-MM-DD HH:mm:ss');
+    let res_item0 = item0.turnon + ':00';
+    let res_item1 = item1.turnon + ':00';
+    let res_item2 = item2.turnon + ':00';
+    let res_item3 = item3.turnon + ':00';
+    let res_item5 = item5.turnon + ':00';
+    let res_item6 = item6.turnon + ':00';
+    let res_item7 = item7.turnon + ':00';
+    let res_item8 = item8.turnon + ':00';
+    let res_off_item0 = item0.turnoff + ':00';
+    let res_off_item1 = item1.turnoff + ':00';
+    let res_off_item2 = item2.turnoff + ':00';
+    let res_off_item3 = item3.turnoff + ':00';
+    let res_off_item5 = item5.turnoff + ':00';
+    let res_off_item6 = item6.turnoff + ':00';
+    let res_off_item7 = item7.turnoff + ':00';
+    let res_off_item8 = item8.turnoff + ':00';
+    if (format_clock === res_item0) {
+      $(".led_status0").text('ON');
+      $(".led_status0").addClass('bg-danger text-white');
+    }
+    if (format_clock === res_item1) {
+      $(".led_status1").text('ON');
+      $(".led_status1").addClass('bg-danger text-white');
+    }
+    if (format_clock === res_item2) {
+      $(".led_status2").text('ON');
+      $(".led_status2").addClass('bg-danger text-white');
+    }
+    if (format_clock === res_item3) {
+      $(".led_status3").text('ON');
+      $(".led_status3").addClass('bg-danger text-white');
+    }
+    if (format_clock === res_item5) {
+      $(".led_status5").text('ON');
+      $(".led_status5").addClass('bg-danger text-white');
+    }
+    if (format_clock === res_item6) {
+      $(".led_status6").text('ON');
+      $(".led_status6").addClass('bg-danger text-white');
+    }
+    if (format_clock === res_item7) {
+      $(".led_status7").text('ON');
+      $(".led_status7").addClass('bg-danger text-white');
+    }
+    if (format_clock === res_item8) {
+      $(".led_status8").text('ON');
+      $(".led_status8").addClass('bg-danger text-white');
+    }
+    // turn off change background button stuatus
+    if (format_clock === res_off_item0) {
+      $(".led_status0").removeClass('bg-danger text-white');
+      $(".led_status0").text('OFF');
+    }
+    if (format_clock === res_off_item1) {
+      $(".led_status1").removeClass('bg-danger text-white');
+      $(".led_status1").text('OFF');
+    }
+    if (format_clock === res_off_item2) {
+      $(".led_status2").removeClass('bg-danger text-white');
+      $(".led_status2").text('OFF');
+    }
+    if (format_clock === res_off_item3) {
+      $(".led_status3").removeClass('bg-danger text-white');
+      $(".led_status3").text('OFF');
+    }
+    if (format_clock === res_off_item5) {
+      $(".led_status5").removeClass('bg-danger text-white');
+      $(".led_status5").text('OFF');
+    }
+    if (format_clock === res_off_item6) {
+      $(".led_status6").removeClass('bg-danger text-white');
+      $(".led_status6").text('OFF');
+    }
+    if (format_clock === res_off_item7) {
+      $(".led_status7").removeClass('bg-danger text-white');
+      $(".led_status7").text('OFF');
+    }
+    if (format_clock === res_off_item8) {
+      $(".led_status8").removeClass('bg-danger text-white');
+      $(".led_status8").text('OFF');
+    }
   }
   autorun();
 
@@ -169,50 +261,50 @@ function Led (){
         <tbody>
         <tr>
           <td className="border"><Icon.Lamp/> Light 1</td>
-          <td className="text-center border"><Button color="primary" className={item0.status==='on'? 'bg-danger text-white' : 'led_status0'} variant="outlined"  onClick={(e)=>Turn_led(item0.status, 'led_status0', 'status')}>{item0.status}</Button></td>
+          <td className="text-center border"><Button color="primary" className={item0.status==='on'? 'bg-danger text-white led_status0' : 'led_status0'} variant="outlined"  onClick={(e)=>Turn_led(item0.status, 'led_status0', 'status')}>{item0.status}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item0.status, 'led_status0', 'turnon')}>{item0.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item0.status, 'led_status0', 'turnoff')}>{item0.turnoff}</Button></td>
         </tr>
         <tr>
           <td className="border"><Icon.Lamp/> Light 2</td>
-          <td className="text-center border"><Button color="primary"  className={item1.status==='on'? 'bg-danger text-white' : ''} variant="outlined"  onClick={(e)=>Turn_led(item1.status, 'led_status1', 'status')}>{item1.status}</Button></td>
+          <td className="text-center border"><Button color="primary"  className={item1.status==='on'? 'bg-danger text-white led_status1' : 'led_status1'} variant="outlined"  onClick={(e)=>Turn_led(item1.status, 'led_status1', 'status')}>{item1.status}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item1.status, 'led_status1', 'turnon')}>{item1.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item1.status, 'led_status1', 'turnoff')}>{item1.turnoff}</Button></td>
         </tr>
         <tr>
           <td className="border"><Icon.Lamp/> Light 3</td>
-          <td className="text-center border"><Button color="primary"  className={item2.status==='on'? 'bg-danger text-white' : ''} variant="outlined"  onClick={(e)=>Turn_led(item2.status, 'led_status2', 'status')}>{item2.status}</Button></td>
+          <td className="text-center border"><Button color="primary"  className={item2.status==='on'? 'bg-danger text-white led_status2' : 'led_status2'} variant="outlined"  onClick={(e)=>Turn_led(item2.status, 'led_status2', 'status')}>{item2.status}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item2.status, 'led_status2', 'turnon')}>{item2.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item2.status, 'led_status2', 'turnoff')}>{item2.turnoff}</Button></td>
         </tr>
         <tr>
           <td className="border"><Icon.Lamp/> Light 4</td>
-          <td className="text-center border"><Button color="primary"  className={item3.status==='on'? 'bg-danger text-white' : ''} variant="outlined"  onClick={(e)=>Turn_led(item3.status, 'led_status3', 'status')}>{item3.status}</Button></td>
+          <td className="text-center border"><Button color="primary"  className={item3.status==='on'? 'bg-danger text-white led_status3' : 'led_status3'} variant="outlined"  onClick={(e)=>Turn_led(item3.status, 'led_status3', 'status')}>{item3.status}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item3.status, 'led_status3', 'turnon')}>{item3.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item3.status, 'led_status3', 'turnoff')}>{item3.turnoff}</Button></td>
         </tr>
         <tr>
           <td className="border"><Icon.Power/> Fan</td>
-          <td className="text-center border"><Button color="primary"  className={item5.status==='on'? 'bg-danger text-white' : ''} variant="outlined"  onClick={(e)=>Turn_led(item5.status, 'led_status5', 'status')}>{item5.status}</Button></td>
+          <td className="text-center border"><Button color="primary"  className={item5.status==='on'? 'bg-danger text-white led_status5' : 'led_status5'} variant="outlined"  onClick={(e)=>Turn_led(item5.status, 'led_status5', 'status')}>{item5.status}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item5.status, 'led_status5', 'turnon')}>{item5.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item5.status, 'led_status5', 'turnoff')}>{item5.turnoff}</Button></td>
         </tr>
         <tr>
           <td className="border"><Icon.Lamp/> Power socket 1</td>
-          <td className="text-center border"><Button color="primary"  className={item6.status==='on'? 'bg-danger text-white' : ''} variant="outlined"  onClick={(e)=>Turn_led(item6.status, 'led_status6', 'status')}>{item6.status}</Button></td>
+          <td className="text-center border"><Button color="primary"  className={item6.status==='on'? 'bg-danger text-white led_status6' : 'led_status6'} variant="outlined"  onClick={(e)=>Turn_led(item6.status, 'led_status6', 'status')}>{item6.status}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item6.status, 'led_status6', 'turnon')}>{item6.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item6.status, 'led_status6', 'turnoff')}>{item6.turnoff}</Button></td>
         </tr>
         <tr>
           <td className="border"><Icon.Lamp/> Power socket 2</td>
-          <td className="text-center border"><Button color="primary"  className={item7.status==='on'? 'bg-danger text-white' : ''} variant="outlined"  onClick={(e)=>Turn_led(item7.status, 'led_status7', 'status')}>{item7.status}</Button></td>
+          <td className="text-center border"><Button color="primary"  className={item7.status==='on'? 'bg-danger text-white led_status7' : 'led_status7'} variant="outlined"  onClick={(e)=>Turn_led(item7.status, 'led_status7', 'status')}>{item7.status}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item7.status, 'led_status7', 'turnon')}>{item7.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item7.status, 'led_status7', 'turnoff')}>{item7.turnoff}</Button></td>
         </tr>
         <tr>
           <td className="border"><Icon.Lamp/> Power socket 3</td>
-          <td className="text-center border"><Button color="primary"  className={item8.status==='on'? 'bg-danger text-white' : ''} variant="outlined"  onClick={(e)=>Turn_led(item8.status, 'led_status8', 'status')}>{item8.status}</Button></td>
-          <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item8.status, 'led_status7', 'turnon')}>{item8.turnon}</Button></td>
+          <td className="text-center border"><Button color="primary"  className={item8.status==='on'? 'bg-danger text-white led_status8' : 'led_status8'} variant="outlined"  onClick={(e)=>Turn_led(item8.status, 'led_status8', 'status')}>{item8.status}</Button></td>
+          <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item8.status, 'led_status8', 'turnon')}>{item8.turnon}</Button></td>
           <td className="text-center border"><Button color="primary" variant="outlined"  onClick={(e)=>setTime(item8.status, 'led_status8', 'turnoff')}>{item8.turnoff}</Button></td>
         </tr>
         </tbody>
